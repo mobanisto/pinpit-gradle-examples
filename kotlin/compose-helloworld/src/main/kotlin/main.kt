97d544211c08bbf1c3abebc305325e04c0b400b8
Sebastian Kürten <sebastian@topobyte.de>
2022-11-29 11:56:23 +0100

    Set title and icon for Windows in Compose examples

--- kotlin/compose-helloworld/src/main/kotlin/main.kt
+++ kotlin/compose-helloworld/src/main/kotlin/main.kt
@@ -7,17 +7,27 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.window.singleWindowApplication
+import androidx.compose.ui.window.Window
+import androidx.compose.ui.window.application
 
-fun main() = singleWindowApplication {
-    var text by remember { mutableStateOf("Hello, World!") }
+fun main() {
+    application {
+        Window(
+            onCloseRequest = ::exitApplication,
+            title = "Pinpit Compose Hello World",
+            icon = painterResource("icon.svg")
+        ) {
+            var text by remember { mutableStateOf("Hello, World!") }
 
-    MaterialTheme {
-        Button(modifier = Modifier.padding(horizontal = 10.dp, vertical = 4.dp), onClick = {
-            text = "Hello, Desktop!"
-        }) {
-            Text(text)
+            MaterialTheme {
+                Button(modifier = Modifier.padding(horizontal = 10.dp, vertical = 4.dp), onClick = {
+                    text = "Hello, Desktop!"
+                }) {
+                    Text(text)
+                }
+            }
         }
     }
 }
